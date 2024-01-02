9649088b710c1a72721ecec74ec058a466e97643
abhishake07 <abhishek.hiyer@gmail.com>
2024-01-02 04:53:44 -0500

    Updating startup.sh file

--- startup.sh
+++ startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ## Just a quick bash script to set up the virtual environment and start Jupyter notebook
 
-source /etc/profile.d/conda.sh
-conda activate cellpose
+source <path/to/conda.sh file> #Please specify path to conda.sh file here and remove "<>"
+conda activate crisprmap-pipeline
 
-jupyter-notebook --no-browser --port=8124 --notebook-dir=/mnt/disks/external/ &
+jupyter-notebook --no-browser --port=8888 --notebook-dir=. & #Please modify notebook dir here
 
 sleep 5
 
 echo "-----------------------------------------------------------------------"
 echo "Please click on the following link:"
-echo $(jupyter notebook list | sed 's/localhost/34.132.91.50/g')
+echo $(jupyter notebook list | sed 's/localhost/<static ip here>/g') ## Please insert static ip here if you are working remote. If working locally, use localhost
 echo "-----------------------------------------------------------------------"
